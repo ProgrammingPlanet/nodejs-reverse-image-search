160e51b2b8de150fff597f70c77d11b52f15e065
Glitch (search-engines) <none>
2021-05-21 08:44:35 +0000

    Updated with Glitch

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            searchs: [
+                {
+                    name: 'Reverse Image Search',
+                    icon: 'img-search.svg',
+                    url: 'img'
+                }
+              ]
         }
     },
     methods: {
