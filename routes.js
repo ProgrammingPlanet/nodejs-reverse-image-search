562dd061b7a0d191541d17565a8688354e0b6b0b
Glitch (search-engines) <none>
2021-05-21 08:13:07 +0000

    🎑📕 Updated with Glitch

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 module.exports = function(app){
 
-    app.get("/", (req,res) => {
-	  	res.sendFile(__dirname + '/public/index.html')
-	});
+  app.get("/", (req,res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  })
+  
+  app.get("/img", (req,res) => {
+    res.sendFile(__dirname + '/public/img.html')
+  })
     
 }
